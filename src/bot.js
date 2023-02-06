66106d19bfa2c7e7784b329e8ba1d5a87c3b02ef
jortiz-testing <jortiz-testing@proton.me>
2023-02-06 00:06:47 -0300

    refactor(bot): Session

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,3 @@
-const { writeFileSync } = require('fs');
 const debug = require('debug')('bot');
 const bot = require('venom-bot');
 const ai = require('./ai');
@@ -8,8 +7,8 @@ module.exports = {
     debug.extend('start')('started');
 
     try {
-      const options = { useChrome: false, logQR: false };
-      const client = await bot.create('zap-gpt', this.save, undefined, options);
+      const options = { session: 'zap-gpt', useChrome: false };
+      const client = await bot.create(options);
 
       await client.onAnyMessage((message) => this.chat(client, message));
     } catch (error) {
@@ -46,18 +45,5 @@ module.exports = {
     debug.extend('chat')('response %O', { response });
 
     await client.sendText(replyTo, ` ${response}`);
-  },
-  save: function (base64) {
-    const matched = base64.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-    const response = {};
-
-    if (matched.length !== 3) {
-      return new Error('Invalid input string');
-    }
-
-    response.type = matched[1];
-    response.data = Buffer.from(matched[2], 'base64');
-
-    writeFileSync('qrcode.png', response.data, 'binary');
   }
 };
